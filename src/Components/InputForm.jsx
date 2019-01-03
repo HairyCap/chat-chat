730fe116145a4c1e09ef8995d257ab2f96de6740
HairyCap <ooooluffy.n.a@gmail.com>
2019-01-03 12:24:19 +0800

    test focus scroll timeout response to better at 200ms

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -41,8 +41,8 @@ class InputFprm extends Component {
         <input
           type="text"
           onChange={e => this.update(e.target.value)}
-          //手机键盘跳出，视图大小调整后滚动
-          onFocus={() => setTimeout(this.props.scroll, 300)}
+          //手机键盘跳出，视图大小调整后滚动,在移动端FireFox测试200毫秒延迟成功率较高（Mi6）
+          onFocus={() => setTimeout(this.props.scroll, 200)}
           //手机键盘点击输入框，滚动到底部
           onClick={() => this.props.scroll()}
           value={this.state.msg.content}
