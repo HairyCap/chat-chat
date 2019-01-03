360771406840f4eef927a6873001625e050b9fbe
HairyCap <ooooluffy.n.a@gmail.com>
2019-01-04 02:13:53 +0800

    add basic msg schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,34 @@
-const graphpl = require("graphql");
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const MsgType = new GraphQLObjectType({
+  name:'Msg',
+  fields:() => ({
+    id:{type: GraphQLString},
+    content:{type: GraphQLString},
+    // fromUser:{type: GraphQLString},
+    // toUser:{type: GraphQLString},
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  field: {
+    msg: {
+      type: MsgType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent,args){
+        //code to get data from db
+      }
+    }
+  }
+})
+// Msg(id:'123'){
+//   conent
+// }
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
