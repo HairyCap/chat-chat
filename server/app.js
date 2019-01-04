30c175abda0215b0b22d269805e26a09112547dc
HairyCap <ooooluffy.n.a@gmail.com>
2019-01-05 02:52:33 +0800

    add cors to server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+//allow cross-origin requests
+app.use(cors());
+
+//connect to mlab database
 mongoose.connect(
   "mongodb://chatchat:chatchat1@ds149404.mlab.com:49404/chat-chat",
   { useNewUrlParser: true }
